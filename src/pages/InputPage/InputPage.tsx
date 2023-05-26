6d098dee2b4057e8184d98255eaf36cf0bf0e4d7
roman <romlink@mail.ru>
2023-05-27 01:00:10 +0300

    custom textField

--- src/pages/InputPage/InputPage.tsx
+++ src/pages/InputPage/InputPage.tsx
@@ -1,4 +1,3 @@
-// import styles from "./ButtonPage.module.css"
 import { ComponentTemplate } from "../../components/ComponentTemplate/ComponentTemplate";
 import { TextField_IU5 } from "../../components/customized/TextField_IU5";
 
@@ -8,7 +7,9 @@ export const InputPage = () => {
     return (
         <div>
             <ComponentTemplate name={'Input'}/>
-            <TextField_IU5></TextField_IU5>
+            <TextField_IU5 label="Фамилия"></TextField_IU5>
+            <TextField_IU5 label="Ошибочное" error={true}></TextField_IU5>
+            <TextField_IU5 label="Заблокированное" disabled value={'Блок'}></TextField_IU5>
         </div>
     )
 }
