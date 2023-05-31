ea01b933e806e515f1d3b55293921bb605c1b08c
roman <romlink@mail.ru>
2023-06-01 01:52:32 +0300

    tabs

--- src/pages/TabsPage/TabsPage.tsx
+++ src/pages/TabsPage/TabsPage.tsx
@@ -1,9 +1,21 @@
 // import styles from "./ButtonPage.module.css"
+import { Tabs_IU5 } from "../../components/customized/Tabs_IU5";
 import { ComponentTemplate } from "../../components/ComponentTemplate/ComponentTemplate";
 
 export const TabsPage = () => {
 
+    const options = [
+        {label: 'Вкладка 1', content: 'Наполнение 1'},
+        {label: 'Вкладка 2', content: 'Наполнение 2'},
+        {label: 'Вкладка 3', content: 'Наполнение 3'}
+    ]
+
     return (
-        <ComponentTemplate name={'Вкладка'}/>
+        <div>
+            <ComponentTemplate name={'Вкладка'}/>
+            <Tabs_IU5
+                options={options}
+            />
+        </div>
     )
 }
